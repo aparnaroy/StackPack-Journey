b6b0667c93eb418669fcf2d277db4212ff946d19
Emilie <emiliea@udel.edu>
2024-04-24 12:29:07 -0400

    platforms, movement of player

--- src/scenes/levelOne.ts
+++ src/scenes/levelOne.ts
@@ -3,7 +3,18 @@ import PhaserLogo from "../objects/phaserLogo";
 import FpsText from "../objects/fpsText";
 
 export default class LevelOne extends Phaser.Scene {
-    fpsText: FpsText;
+    private player?: Phaser.Physics.Arcade.Sprite;
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
+    private key?: Phaser.GameObjects.Sprite;
+    private platforms?: Phaser.Physics.Arcade.StaticGroup;
+    private banana?: Phaser.GameObjects.Sprite;
+    private mushroom?: Phaser.GameObjects.Sprite;
+    private vineItem?: Phaser.GameObjects.Sprite;
+    private stone?: Phaser.GameObjects.Sprite;
+    private ground?: Phaser.Physics.Arcade.Image;
+
+    private lastDirection: string = "right";
+    private isColliding: boolean = false;
 
     constructor() {
         super({ key: "Level1" });
@@ -14,6 +25,76 @@ export default class LevelOne extends Phaser.Scene {
             "level1Background",
             "assets/level1/Level1Background.jpg"
         );
+        this.load.image("stackpack", "assets/stackpack.png");
+        this.load.spritesheet("key", "assets/key.png", {
+            frameWidth: 768 / 24,
+            frameHeight: 32,
+        });
+
+        this.load.spritesheet("gal_right", "assets/Pink_Monster_Walk_6.png", {
+            frameWidth: 128,
+            frameHeight: 128,
+        });
+        this.load.spritesheet(
+            "gal_left",
+            "assets/Pink_Monster_Walk_Left6.png",
+            {
+                frameWidth: 128,
+                frameHeight: 128,
+            }
+        );
+        this.load.spritesheet(
+            "gal_idle_right",
+            "assets/Pink_Monster_Idle_4.png",
+            {
+                frameWidth: 128,
+                frameHeight: 128,
+            }
+        );
+        this.load.spritesheet(
+            "gal_idle_left",
+            "assets/Pink_Monster_Idle_Left4.png",
+            {
+                frameWidth: 128,
+                frameHeight: 128,
+            }
+        );
+        this.load.spritesheet(
+            "gal_jump_right",
+            "assets/Pink_Monster_Jump_8.png",
+            {
+                frameWidth: 128,
+                frameHeight: 128,
+            }
+        );
+        this.load.spritesheet("gal_climb", "assets/Pink_Monster_Climb_4.png", {
+            frameWidth: 128,
+            frameHeight: 128,
+        });
+        this.load.spritesheet(
+            "gal_hurt_right",
+            "assets/Pink_Monster_Hurt_4.png",
+            {
+                frameWidth: 128,
+                frameHeight: 128,
+            }
+        );
+        this.load.spritesheet(
+            "gal_hurt_left",
+            "assets/Pink_Monster_Hurt_Left4.png",
+            {
+                frameWidth: 128,
+                frameHeight: 128,
+            }
+        );
+        this.load.image("banana", "assets/level1/banana.png");
+        this.load.image("bananaBubble", "assets/level1/bananaBubble.png");
+        this.load.image("LgPlatform", "assets/level1/LgPlatform.png");
+        this.load.image("MdPlatform", "assets/level1/MdPlatform.png");
+        this.load.image("SmPlatform", "assets/level1/SmPlatform.png");
+        this.load.image("monkey", "assets/level1/monkey.png");
+        this.load.image("mushroom", "assets/level1/mushroom.png");
+        this.load.image("vineItem", "assets/level1/vineItem.png");
     }
 
     create() {
@@ -24,7 +105,154 @@ export default class LevelOne extends Phaser.Scene {
             this.cameras.main.width / backgroundImage.width,
             this.cameras.main.height / backgroundImage.height
         );
+
+        const stackpack = this.add
+            .image(0, 0, "stackpack")
+            .setPosition(1170, 165);
+        stackpack.setScale(0.26, 0.26);
+
+        this.player = this.physics.add
+            .sprite(100, 450, "gal_right")
+            .setScale(0.77, 0.77)
+            .setOrigin(0.5, 0.5);
+        this.player.setCollideWorldBounds(true);
+
+        // KEY ANIM
+        this.anims.create({
+            key: "turn",
+            frames: this.anims.generateFrameNumbers("key", {
+                start: 0,
+                end: 25,
+            }),
+            frameRate: 8,
+            repeat: -1,
+        });
+
+        // PLAYER ANIMS
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers("gal_right", {
+                start: 0,
+                end: 5,
+            }),
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "gal_right", frame: 1 }],
+        });
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("gal_left", {
+                start: 0,
+                end: 5,
+            }),
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "idle_right",
+            frames: this.anims.generateFrameNumbers("gal_idle_right", {
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "idle_left",
+            frames: this.anims.generateFrameNumbers("gal_idle_left", {
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "jump_right",
+            frames: this.anims.generateFrameNumbers("gal_jump_right", {
+                start: 0,
+                end: 7,
+            }),
+        });
+        this.anims.create({
+            key: "climb",
+            frames: this.anims.generateFrameNumbers("gal_climb", {
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 15,
+        });
+        this.anims.create({
+            key: "hurt_right",
+            frames: this.anims.generateFrameNumbers("gal_hurt_right", {
+                start: 0,
+                end: 1,
+            }),
+            frameRate: 10,
+            repeat: 0,
+        });
+        this.anims.create({
+            key: "hurt_left",
+            frames: this.anims.generateFrameNumbers("gal_hurt_left", {
+                start: 4,
+                end: 2,
+            }),
+            frameRate: 10,
+            repeat: 0,
+        });
+
+        this.cursors = this.input.keyboard?.createCursorKeys();
+
+        // PLATFORMS
+        this.platforms = this.physics.add.staticGroup();
+        this.ground = this.platforms.create(
+            600,
+            790,
+            "LgPlatform"
+        ) as Phaser.Physics.Arcade.Image;
+        this.ground.setSize(this.ground.width + 1100, this.ground.height - 370);
+        //this.ground.setScale(5).refreshBody();
+        //this.ground.setAlpha(0);
+
+        const platform1 = this.platforms
+            .create(350, 585, "SmPlatform")
+            .setScale(1, 1);
+        platform1.setSize(platform1.width - 110, platform1.height - 490);
+
+        this.physics.add.collider(this.player, this.platforms);
+        this.key = this.add.sprite(1200, 650, "key").setScale(2.5, 2.5);
     }
 
-    update() {}
+    update() {
+        // KEY TURN
+        if (this.key) {
+            this.key.anims.play("turn", true);
+        }
+
+        // PLAYER ANIMS
+        if (this.player && this.cursors) {
+            if (!this.isColliding) {
+                if (this.cursors.up.isDown && this.player.body?.touching.down) {
+                    this.player.anims.play("jump_right", true);
+                    this.player.setVelocityY(-530);
+                } else if (this.cursors.right.isDown) {
+                    this.player.setVelocityX(290);
+                    this.player.anims.play("right", true);
+                    this.lastDirection = "right"; // Update last direction
+                } else if (this.cursors.left.isDown) {
+                    this.player.setVelocityX(-290);
+                    this.player.anims.play("left", true);
+                    this.lastDirection = "left"; // Update last direction
+                } else {
+                    this.player.setVelocityX(0);
+                    // Check last direction and play corresponding idle animation
+                    if (this.lastDirection === "right") {
+                        this.player.anims.play("idle_right", true);
+                    } else {
+                        this.player.anims.play("idle_left", true);
+                    }
+                }
+            }
+        }
+    }
 }
